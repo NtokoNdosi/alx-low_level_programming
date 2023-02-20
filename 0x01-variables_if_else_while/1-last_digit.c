9789e8072cbf204005a2f59b4531c54b87cb2e3a
NtokoNdosi <zanacele4@com>
2023-02-20 09:56:11 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if (lastd == 0)
 	{
-	printf("last digit of %d is %d and is 0\n", n, lastd);
+	printf("last digit of %d is %d and is zero\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
